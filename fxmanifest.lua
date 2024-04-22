1d38e02dee8e58cbecd3d1d69d334ffcee6f91be
EP <ep.kouhia@gmail.com>
2024-04-22 23:57:47 +0300

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version   'cerulean'
+game         'gta5'
+
+author 'EP'
+description 'Methlab shell based on the methlab MLO in the game'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/ep_methlab.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ep_methlab.ytyp'
